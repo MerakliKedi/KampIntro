8e25e39f0ae4e01cf8c5679d36399fc0f1cd06c5
abdullah yayar <abdullah_yayar@hotmail.com>
2021-03-16 22:06:39 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.KursunEgitmeni = "Abdullah Yayar";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Python";
+            kurs2.KursunEgitmeni = "Zehra Yayar";
+            kurs2.IzlenmeOrani = 88;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Java";
+            kurs3.KursunEgitmeni = "Fatma Nur Yayar";
+            kurs3.IzlenmeOrani = 98;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "React";
+            kurs4.KursunEgitmeni = "Muhammed Ali Yayar";
+            kurs4.IzlenmeOrani = 100;
+
+
+           // Console.WriteLine(kurs1.KursAdi+ ":" + kurs1.KursunEgitmeni);
+
+            Kurs[] kurslar= new Kurs[] {kurs1, kurs2,kurs3,kurs4 };
+
+            foreach (Kurs kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi+ ":"+kurs.KursunEgitmeni);
+            }
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string KursunEgitmeni { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DO NOT REPEAT YOURSELF
+            //degiskenler veri tutuculardır.
+            //type safety= tip güvenliği
+            
+            string kategoriEtiketi = "Kategoriler!";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisyapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGirisyapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları Menüsü");
+
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Menüsü");
+            }
+            
+            Console.WriteLine(kategoriEtiketi);
+            
+        }
+    }
+}
